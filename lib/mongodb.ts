717e0400367c1bfc6b2ef9fc6f3e7273cab370bc
Knownasniell <levrouant@gmail.com>
2026-04-27 11:07:29 +0800

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -9,13 +9,22 @@ import { MongoClient } from "mongodb"
 const MONGODB_URI =
   "mongodb+srv://levrouniverse_db_user:n6UOtilteK7rzv2j@nourv6s.dbpznrd.mongodb.net/ver6core?retryWrites=true&w=majority"
 
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is missing")
+}
+
 let client: MongoClient
-let clientPromise: Promise<MongoClient>
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient>
+}
 
 if (!global._mongoClientPromise) {
-  client = new MongoClient(MONGO_URI, options)
+  client = new MongoClient(MONGODB_URI)
   global._mongoClientPromise = client.connect()
 }
-clientPromise = global._mongoClientPromise
+
+const clientPromise = global._mongoClientPromise
 
 export default clientPromise
